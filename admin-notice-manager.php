ba1b9038110c0e585ac8e9c6397c0e94456f4f87
Your Name <mdasifiqbal141@gmail.com>
2026-08-14 14:58:54 +0600

    Update admin notice manager function

--- admin-notice-manager.php
+++ admin-notice-manager.php
@@ -1,14 +1,32 @@
 <?php
 /**
  * Plugin Name: Admin Notice Manager
- * Description: A simple plugin to display custom notices in the WordPress dashboard.
+ * Description: A Simple plugin to display custom notics in the WordPress dashboard
  * Version: 1.0.0
- * Author: Your Name
- * License: GPL v2 or later
+ * Author: Md Asif Ikbal
+ * License: GPL v2 
  * Text Domain: admin-notice-manager
  */
 
-// Exit if accessed directly (Security Best Practice)
-if ( ! defined( 'ABSPATH' ) ) {
+// ( Security Best Practice ) 
+ if ( ! defined( 'ABSPATH'  ) ) {
     exit;
+ }
+
+
+ // Display a Custom Notice In  The Admin Area
+
+function anm_display_custom_notice( ) {
+
+    ?>
+            <div class=" notice notice-sucess is-dismissble">
+                <p>  
+                    <strong><?php  esc_html_e( 'Hello! This is your custom Admin Notice Manaer is working' , 'admin-notice-manager'); ?> 
+
+                    </strong>
+                </p>
+
+            </div>
+    <?php
 }
+add_action( 'admin_notices', 'anm_display_custom_notice' );
